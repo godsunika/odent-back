8e62266ebfb8ddf5134fdcd3d7469a6d5d0f971e
godsunika <103250935+godsunika@users.noreply.github.com>
2022-10-12 15:43:51 +0700

    fix db pt 2

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@ const dbConfig  = require("../config/db.config");
 const Sequelize = require("sequelize")
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host            : dbConfig.HOST,
+  port            : dbConfig.PORT,
   dialect         : dbConfig.dialect,
   operatorsAliases: false,
   pool            : {
